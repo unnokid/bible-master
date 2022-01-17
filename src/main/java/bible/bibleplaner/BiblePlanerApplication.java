b6f3df3e3e2dd9749656f49d858a51e8eb70391d
unnokid <kimki0609@gmail.com>
2022-01-17 23:37:35 +0900

    Initial commit

---
+++ src/main/java/bible/bibleplaner/BiblePlanerApplication.java
@@ -0,0 +1,13 @@
+package bible.bibleplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblePlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblePlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bible/bibleplaner/BiblePlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package bible.bibleplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblePlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
