e31bca0556e4ad73b1e9dbcf969d7972dd4ee938
unnokid <kimki0609@gmail.com>
2022-01-21 23:21:01 +0900

    Add BibleController.java

--- src/main/java/bible/bibleplanner/bible/BibleController.java
+++ src/main/java/bible/bibleplanner/bible/BibleController.java
@@ -1,4 +1,28 @@
 package bible.bibleplanner.bible;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class BibleController {
+
+    private BibleService bibleService;
+
+    @Autowired
+    public BibleController(BibleService bibleService) {
+        this.bibleService = bibleService;
+    }
+    
+    //요청처리
+    @GetMapping("/bibles")
+    public String list(Model model){
+        List<Bible> bibles = bibleService.findBibles();
+        model.addAttribute("bibles",bibles);
+        return "list";
+    }
 }
