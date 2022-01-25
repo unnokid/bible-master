5001a18c92f524814fd3d431062b019d19c5dc63
unnokid <kimki0609@gmail.com>
2022-01-25 20:29:46 +0900

    Update BibleRepository.java

--- src/main/java/bible/bibleplanner/bible/BibleRepository.java
+++ src/main/java/bible/bibleplanner/bible/BibleRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+
 @Repository
 public interface BibleRepository {
 
     List<Bible> findAll();
+    List<Bible> searchAll();
 }
