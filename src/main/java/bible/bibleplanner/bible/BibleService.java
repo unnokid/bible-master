3d568f518bf45c58a30579f1c836d524903488de
unnokid <kimki0609@gmail.com>
2022-01-25 20:29:55 +0900

    Update BibleService.java

--- src/main/java/bible/bibleplanner/bible/BibleService.java
+++ src/main/java/bible/bibleplanner/bible/BibleService.java
@@ -1,16 +1,22 @@
 package bible.bibleplanner.bible;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class BibleService {
     private BibleRepository bibleRepository;
 
 
     public BibleService(BibleRepository bibleRepository) {
         this.bibleRepository = bibleRepository;
+    }
+
+    public List<Bible> searchBibles(){
+        return bibleRepository.searchAll();
     }
 
     public List<Bible> findBibles(){
